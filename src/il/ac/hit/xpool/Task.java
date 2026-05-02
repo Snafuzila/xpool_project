0d325767faad1f9e10ccd9c0b4b9fcfd843df646
Snafuzila <Boazattiya8@gmail.com>
2026-05-02 22:46:26 +0300

    changed comment in interface

--- src/il/ac/hit/xpool/Task.java
+++ src/il/ac/hit/xpool/Task.java
@@ -10,6 +10,6 @@ public interface Task {
 Technical Note on Dynamic Priority:
 "The library uses a PriorityQueue for efficient task management (O(log n)).
 As per Java's standard behavior, changing a task's priority while it is inside the queue does not trigger a re-sort.
- To handle dynamic changes, I have provided the changePriority(Task, int) method in the ThreadsPool class -
+ To handle dynamic changes, I have provided the updateTaskPriority(Task, int) method in the ThreadsPool class -
  which ensures the internal heap is correctly updated."
  */
